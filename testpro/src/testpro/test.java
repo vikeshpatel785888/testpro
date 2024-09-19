3c7844703852a5790ac58c62c705e00ffcc9cdfc
cutm <vikeshpatel@gmail.com>
2024-09-20 00:54:04 +0530

    added classtest.java

---
+++ testpro/src/testpro/test.java
@@ -0,0 +1,7 @@
+package testpro;
+
+public class test {
+	public static void main (String[]args) {
+	System.out.println("Mr vikesh patel is the big man of the world .You were know yaa dont know about that person you wil be know na");
+	}
+}
